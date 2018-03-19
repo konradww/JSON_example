05bb61ec05d5263babecaca8e0bff23c547694ec
Konrad <marek.dudek@protonmail.ch>
2018-03-19 20:16:40 +0100

    define Data

---
+++ JSONexample.py
@@ -0,0 +1,10 @@
+# Read and write JSON files with Python 3
+# -*- coding: utf-8 -*-
+import json
+
+# Define data
+data = {'string': 'example string',
+        'list': [75, 42, 3.141, 777, 'text'],
+        'dict': {'key1': 'value1',
+                'key2': 'value2',
+                'key3': 3}}
