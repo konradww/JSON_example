fec014bfbde3e0532399acb533039e26839c8e1d
Konrad <marek.dudek@protonmail.ch>
2018-03-19 20:24:03 +0100

    I writed JSON

--- JSONexample.py
+++ JSONexample.py
@@ -1,6 +1,7 @@
 # Read and write JSON files with Python 3
 # -*- coding: utf-8 -*-
 import json
+import io
 
 # Define data
 data = {'string': 'example string',
@@ -8,3 +9,8 @@ data = {'string': 'example string',
         'dict': {'key1': 'value1',
                 'key2': 'value2',
                 'key3': 3}}
+
+
+# Write JSON file
+with io.open('data.json', 'w') as outfile:
+    outfile.write(json.dumps(data))
